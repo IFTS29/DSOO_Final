3be35f4529103386ca49d298e987dbf3b195c6b2
mariana <mariana.aiello89@gmail.com>
2025-08-03 23:58:26 -0300

    creación de formulario frmConexion y cambios en clase Conexion.cs

--- ProyectoIntegrador/Program.cs
+++ ProyectoIntegrador/Program.cs
@@ -16,7 +16,7 @@ namespace ProyectoIntegrador
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmLogin());
+            Application.Run(new frmConexion());
         }
     }
 }
---
+++ ProyectoIntegrador/frmConexion.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace ProyectoIntegrador
+{
+    partial class frmConexion
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTituloLogin = new System.Windows.Forms.Label();
+            this.lblServidor = new System.Windows.Forms.Label();
+            this.lblPuerto = new System.Windows.Forms.Label();
+            this.lblUsuario = new System.Windows.Forms.Label();
+            this.lblClave = new System.Windows.Forms.Label();
+            this.btnSalirLogin = new System.Windows.Forms.Button();
+            this.txtServidor = new System.Windows.Forms.TextBox();
+            this.txtPuerto = new System.Windows.Forms.TextBox();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.txtClave = new System.Windows.Forms.TextBox();
+            this.btnConectar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTituloLogin
+            // 
+            this.lblTituloLogin.AutoSize = true;
+            this.lblTituloLogin.BackColor = System.Drawing.Color.White;
+            this.lblTituloLogin.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTituloLogin.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.lblTituloLogin.Location = new System.Drawing.Point(181, 71);
+            this.lblTituloLogin.Name = "lblTituloLogin";
+            this.lblTituloLogin.Size = new System.Drawing.Size(302, 25);
+            this.lblTituloLogin.TabIndex = 1;
+            this.lblTituloLogin.Text = "CONEXIÓN A LA BASE DE DATOS";
+            // 
+            // lblServidor
+            // 
+            this.lblServidor.AutoSize = true;
+            this.lblServidor.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblServidor.Location = new System.Drawing.Point(52, 167);
+            this.lblServidor.Name = "lblServidor";
+            this.lblServidor.Size = new System.Drawing.Size(69, 17);
+            this.lblServidor.TabIndex = 2;
+            this.lblServidor.Text = "SERVIDOR";
+            // 
+            // lblPuerto
+            // 
+            this.lblPuerto.AutoSize = true;
+            this.lblPuerto.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPuerto.Location = new System.Drawing.Point(66, 228);
+            this.lblPuerto.Name = "lblPuerto";
+            this.lblPuerto.Size = new System.Drawing.Size(56, 17);
+            this.lblPuerto.TabIndex = 3;
+            this.lblPuerto.Text = "PUERTO";
+            // 
+            // lblUsuario
+            // 
+            this.lblUsuario.AutoSize = true;
+            this.lblUsuario.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUsuario.Location = new System.Drawing.Point(360, 172);
+            this.lblUsuario.Name = "lblUsuario";
+            this.lblUsuario.Size = new System.Drawing.Size(64, 17);
+            this.lblUsuario.TabIndex = 4;
+            this.lblUsuario.Text = "USUARIO";
+            // 
+            // lblClave
+            // 
+            this.lblClave.AutoSize = true;
+            this.lblClave.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblClave.Location = new System.Drawing.Point(360, 228);
+            this.lblClave.Name = "lblClave";
+            this.lblClave.Size = new System.Drawing.Size(45, 17);
+            this.lblClave.TabIndex = 5;
+            this.lblClave.Text = "CLAVE";
+            // 
+            // btnSalirLogin
+            // 
+            this.btnSalirLogin.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalirLogin.Location = new System.Drawing.Point(568, 25);
+            this.btnSalirLogin.Name = "btnSalirLogin";
+            this.btnSalirLogin.Size = new System.Drawing.Size(69, 37);
+            this.btnSalirLogin.TabIndex = 6;
+            this.btnSalirLogin.Text = "Salir";
+            this.btnSalirLogin.UseVisualStyleBackColor = true;
+            this.btnSalirLogin.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // txtServidor
+            // 
+            this.txtServidor.BackColor = System.Drawing.Color.Snow;
+            this.txtServidor.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtServidor.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.txtServidor.Location = new System.Drawing.Point(135, 164);
+            this.txtServidor.Name = "txtServidor";
+            this.txtServidor.Size = new System.Drawing.Size(177, 25);
+            this.txtServidor.TabIndex = 7;
+            this.txtServidor.Text = "Ej. localhost";
+            this.txtServidor.Enter += new System.EventHandler(this.txtServidor_Enter);
+            this.txtServidor.Leave += new System.EventHandler(this.txtServidor_Leave);
+            // 
+            // txtPuerto
+            // 
+            this.txtPuerto.BackColor = System.Drawing.Color.Snow;
+            this.txtPuerto.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPuerto.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.txtPuerto.Location = new System.Drawing.Point(135, 225);
+            this.txtPuerto.Name = "txtPuerto";
+            this.txtPuerto.Size = new System.Drawing.Size(177, 25);
+            this.txtPuerto.TabIndex = 8;
+            this.txtPuerto.Text = "Ej. 3306";
+            this.txtPuerto.Enter += new System.EventHandler(this.txtPuerto_Enter);
+            this.txtPuerto.Leave += new System.EventHandler(this.txtPuerto_Leave);
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.BackColor = System.Drawing.Color.Snow;
+            this.txtUsuario.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUsuario.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.txtUsuario.Location = new System.Drawing.Point(431, 164);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(177, 25);
+            this.txtUsuario.TabIndex = 9;
+            this.txtUsuario.Text = "Ej. root";
+            this.txtUsuario.Enter += new System.EventHandler(this.txtUsuario_Enter);
+            this.txtUsuario.Leave += new System.EventHandler(this.txtUsuario_Leave);
+            // 
+            // txtClave
+            // 
+            this.txtClave.BackColor = System.Drawing.Color.Snow;
+            this.txtClave.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtClave.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.txtClave.Location = new System.Drawing.Point(431, 225);
+            this.txtClave.Name = "txtClave";
+            this.txtClave.Size = new System.Drawing.Size(177, 25);
+            this.txtClave.TabIndex = 10;
+            this.txtClave.Text = "Ej. ******";
+            this.txtClave.Enter += new System.EventHandler(this.txtClave_Enter);
+            this.txtClave.Leave += new System.EventHandler(this.txtClave_Leave);
+            // 
+            // btnConectar
+            // 
+            this.btnConectar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(240)))), ((int)(((byte)(195)))));
+            this.btnConectar.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConectar.Location = new System.Drawing.Point(250, 305);
+            this.btnConectar.Name = "btnConectar";
+            this.btnConectar.Size = new System.Drawing.Size(154, 42);
+            this.btnConectar.TabIndex = 11;
+            this.btnConectar.Text = "CONECTAR";
+            this.btnConectar.UseVisualStyleBackColor = false;
+            this.btnConectar.Click += new System.EventHandler(this.btnConectar_Click);
+            // 
+            // frmConexion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(665, 392);
+            this.Controls.Add(this.btnConectar);
+            this.Controls.Add(this.txtClave);
+            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.txtPuerto);
+            this.Controls.Add(this.txtServidor);
+            this.Controls.Add(this.btnSalirLogin);
+            this.Controls.Add(this.lblClave);
+            this.Controls.Add(this.lblUsuario);
+            this.Controls.Add(this.lblPuerto);
+            this.Controls.Add(this.lblServidor);
+            this.Controls.Add(this.lblTituloLogin);
+            this.Name = "frmConexion";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CONEXIÓN A BASE DE DATOS";
+            this.Load += new System.EventHandler(this.frmConexion_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTituloLogin;
+        private System.Windows.Forms.Label lblServidor;
+        private System.Windows.Forms.Label lblPuerto;
+        private System.Windows.Forms.Label lblUsuario;
+        private System.Windows.Forms.Label lblClave;
+        private System.Windows.Forms.Button btnSalirLogin;
+        private System.Windows.Forms.TextBox txtServidor;
+        private System.Windows.Forms.TextBox txtPuerto;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.TextBox txtClave;
+        private System.Windows.Forms.Button btnConectar;
+    }
+}
